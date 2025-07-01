670ab2bb1919ab4c1f772018d3b7f79882d2ddc4
Abhi-2577 <maneabhishek2577@gmail.com>
2025-07-01 11:24:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// const BASE_URL ="https://cdn.jsdelivr.net/gh/fawazahned0/currency-api@1/latest/currencies/usd/inr.json";
+// const dropdownSelect = document.querySelectorAll(".dropdown select");
+
+// for(let select of dropdowns){
+//     for(code in countryList){
+//         let newOption = document.createElement("option");
+//         newOption.innerText=currCode;
+//         newOption.value= currcode;
+//         select.append(newOption);
+//     }
+// }
